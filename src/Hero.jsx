6058d621b7715609a6c5037fa1e948cf95d7c1cf
Umarah Gul <umarahgul@gmail.com>
2026-05-04 11:48:24 +0500

    Update the Hero page style

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,8 +1,11 @@
 export default function Hero() {
   return (
-    <section>
-      <h1>Welcome to my Portfolio</h1>
-      <p>This is the hero section</p>
+    <section style={{ padding: "100px 20px", textAlign: "center" }}>
+      <h1 style={{ fontSize: "48px", marginBottom: "20px" }}>
+        Umarah Gul
+        </h1>
+      <p  style={{ fontSize: "20px", opacity: 0.8 }}>
+        React Developer | Building Modern Web Apps</p>
     </section>
   );
 }
