d9c61d7e4c4ec85689ec738522f5b3f60d569086
Umarah Gul <umarahgul@gmail.com>
2026-05-04 11:44:01 +0500

    Update About section and index.css

--- src/About.jsx
+++ src/About.jsx
@@ -1,8 +1,13 @@
 export default function About() {
   return (
-    <section>
-      <h2>About Me</h2>
-      <p>I am building my React portfolio.</p>
+    <section style={{padding: "60px 20px", textAlign: 'center'}}>
+         <h2 style={{ fontSize: "32px", marginBottom: "20px" }}>
+            About Me
+            </h2>
+      
+      <p style={{ maxWidth: "600px", margin: "0 auto" }}>
+        I am building my React portfolio.
+        </p>
     </section>
   );
 }
